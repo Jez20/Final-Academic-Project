8209f0c87449dce0132fbf4f117cba19dffef8e7
Feivel Doctora <90942345+DoctoraCics@users.noreply.github.com>
2022-05-22 20:00:24 +0800

    added filter functionalities

--- src/java/Model/DatabaseManager.java
+++ src/java/Model/DatabaseManager.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -21,11 +22,14 @@ public class DatabaseManager {
 
     private Connection conn;
     private Security encryptDecrypt;
+    private Queries query;
+    private ConstantCateg savedCategSizeandGender;
 
     public DatabaseManager(String url, String userName, String pass, String dbDriver, String key) {
         try {
             this.conn = DriverManager.getConnection(url, userName, pass);
             this.encryptDecrypt = new Security(key);
+            this.savedCategSizeandGender = new ConstantCateg(returnSizeCateg(), returnGenderCateg());
             Class.forName(dbDriver);
         } catch (SQLException e) {
             e.printStackTrace();
@@ -36,8 +40,8 @@ public class DatabaseManager {
 
     public ResultSet returnSizeCateg() {
         try {
-            String query = "SELECT * FROM PRODUCT_SIZE_TABLE";
-            PreparedStatement statement = this.conn.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            query = Queries.valueOf("returnSizeCateg");
+            PreparedStatement statement = this.conn.prepareStatement(query.getQuery(), ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             return statement.executeQuery();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -47,8 +51,8 @@ public class DatabaseManager {
 
     public ResultSet returnGenderCateg() {
         try {
-            String query = "SELECT * FROM PRODUCT_GENDER_TABLE";
-            PreparedStatement statement = this.conn.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            query = Queries.valueOf("returnGenderCateg");
+            PreparedStatement statement = this.conn.prepareStatement(query.getQuery(), ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             return statement.executeQuery();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -56,25 +60,86 @@ public class DatabaseManager {
         }
     }
 
+    public ResultSet applyFilter(HttpServletRequest request) {
+
+        query = Queries.valueOf("querywithSize");
+        int[] priceRange = new int[]{0, 999999};
+        String selectedPriceRange = request.getParameter("price");
+        boolean withOutSize = request.getParameter("size").equals("");
+        if (withOutSize) {
+            query = Queries.valueOf("querywithOutSize");
+        }
+        switch (selectedPriceRange) {
+            case "100-300":
+                priceRange[0] = 100;
+                priceRange[1] = 300;
+                break;
+            case "300-600":
+                priceRange[0] = 300;
+                priceRange[1] = 600;
+                break;
+            case "600-900":
+                priceRange[0] = 600;
+                priceRange[1] = 900;
+                break;
+            case "900-1200":
+                priceRange[0] = 900;
+                priceRange[1] = 1200;
+                break;
+            case "1200-1500":
+                priceRange[0] = 1200;
+                priceRange[1] = 999999;
+                break;
+        }
+        if (withOutSize) {
+            try {
+                PreparedStatement stmt = this.conn.prepareStatement(query.getQuery(), ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+                stmt.setString(1, "%" + request.getParameter("gender") + "%");
+                stmt.setInt(2, priceRange[0]);
+                stmt.setInt(3, priceRange[1]);
+                stmt.setString(4, "%" + request.getParameter("university") + "%");
+                ResultSet rs = stmt.executeQuery();
+                return rs;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        } else {
+            try {
+                PreparedStatement stmt = this.conn.prepareStatement(query.getQuery(), ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+                stmt.setString(1, "%" + request.getParameter("gender") + "%");
+                stmt.setInt(2, priceRange[0]);
+                stmt.setInt(3, priceRange[1]);
+                stmt.setString(4, "%" + request.getParameter("university") + "%");
+                stmt.setString(5, request.getParameter("size"));
+                ResultSet rs = stmt.executeQuery();
+                return rs;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
     public ResultSet returnproductSchool(HttpServletRequest request) {
-        String query = "SELECT DISTINCT\n"
-                + "PRODUCT_VARIANT_TABLE.PRODUCT_ID,\n"
-                + "PRODUCT_TABLE.PRODUCT_NAME,\n"
-                + "PRODUCT_TABLE.PRODUCT_DESC,\n"
-                + "PRODUCT_TABLE.PRODUCT_SCHOOL,\n"
-                + "PRODUCT_TABLE.PRODUCT_IMG_LINK,\n"
-                + "PRODUCT_GENDER_TABLE.PRODUCT_GENDER,\n"
-                + "PRODUCT_SIZE_TABLE.PRODUCT_SIZE,\n"
-                + "PRODUCT_VARIANT_TABLE.PRODUCT_PRICE,\n"
-                + "PRODUCT_VARIANT_TABLE.PRODUCT_STOCK\n"
-                + "FROM PRODUCT_VARIANT_TABLE\n"
-                + "JOIN PRODUCT_TABLE ON PRODUCT_TABLE.PRODUCT_ID = PRODUCT_VARIANT_TABLE.PRODUCT_ID\n"
-                + "JOIN PRODUCT_GENDER_TABLE ON PRODUCT_VARIANT_TABLE.PRODUCT_GENDER_ID = PRODUCT_GENDER_TABLE.PRODUCT_GENDER_ID\n"
-                + "JOIN PRODUCT_SIZE_TABLE ON PRODUCT_SIZE_TABLE.PRODUCT_SIZE_ID = PRODUCT_VARIANT_TABLE.PRODUCT_SIZE_ID\n"
-                + "AND PRODUCT_TABLE.PRODUCT_SCHOOL = ? ";
+        query = Queries.valueOf("returnproductSchool");
         try {
-            PreparedStatement stmt = this.conn.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            PreparedStatement stmt = this.conn.prepareStatement(query.getQuery(), ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             stmt.setString(1, request.getParameter("action"));
+            ResultSet rs = stmt.executeQuery();
+            return rs;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public ResultSet returnProductDetail(HttpServletRequest request) {
+        query = Queries.valueOf("specificProduct");
+        try {
+            PreparedStatement stmt = this.conn.prepareStatement(query.getQuery(), ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            stmt.setString(1, request.getParameter("action"));
+            stmt.setString(2, request.getParameter("school"));
             ResultSet rs = stmt.executeQuery();
             return rs;
         } catch (Exception e) {
@@ -98,9 +163,8 @@ public class DatabaseManager {
                     break;
             }
             if (match) {
-                String query = "INSERT INTO USER_TABLE (USER_PASSWORD, USER_ROLE, USER_EMAIL) "
-                        + "VALUES (?, ?, ?)";
-                PreparedStatement stmt = this.conn.prepareStatement(query);
+                query = Queries.valueOf("signUp");
+                PreparedStatement stmt = this.conn.prepareStatement(query.getQuery());
                 stmt.setString(1, this.encryptDecrypt.encrypt(password));
                 stmt.setString(2, role);
                 stmt.setString(3, request.getParameter("email").trim());
@@ -118,8 +182,8 @@ public class DatabaseManager {
         try {
             HttpSession session = request.getSession();
 
-            String query = "SELECT * FROM USER_TABLE WHERE USER_EMAIL = ?";
-            PreparedStatement stmt = this.conn.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            query = Queries.valueOf("logIn");
+            PreparedStatement stmt = this.conn.prepareStatement(query.getQuery(), ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             stmt.setString(1, request.getParameter("email"));
             ResultSet rs = stmt.executeQuery();
 
@@ -146,4 +210,26 @@ public class DatabaseManager {
             return false;
         }
     }
+
+    public void printResultSets(ResultSet rs) {
+        try {
+            System.out.println("printing result set: ");
+            ResultSetMetaData rsMd = rs.getMetaData();
+            int columnNumber = rsMd.getColumnCount();
+            int entityCount = 0;
+            while (rs.next()) {
+                for (int i = 1; i <= columnNumber; i++) {
+                    System.out.println(String.format("%s: %s", rsMd.getColumnLabel(i), rs.getString(i)));
+                }
+                System.out.println("");
+                entityCount++;
+            }
+            System.out.println(String.format("Number of Entities in the Result set: %d", entityCount));
+            rs.beforeFirst();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
